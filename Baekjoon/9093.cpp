f7131b600411be1ef3f9e34ce49c1e1b6c2bbe32
심우진 <ggoggal@gmail.com>
2023-02-13 14:55:21 +0900

    20230213

---
+++ Baekjoon/9093.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+int main()
+{
+	//갯수 및 갯수만큼 문자열 입력받기
+	int num = 0;
+
+	std::cin >> num;
+
+	char** sentences = new char*[num];
+	char** sentences_result = new char* [num];
+
+	for (int i = 0; i < num; i++)
+	{
+		sentences[i] = new char[1000];
+		sentences_result[i] = new char[1000];
+	}
+
+	std::cin.ignore();
+
+	for (int i = 0; i < num; i++)
+	{
+		std::cin.getline(sentences[i], 1000);
+	}
+
+	//실제 내용
+	//TODO::
+	std::stack<char> reverse;
+
+	for (int i = 0; i < num; i++)
+	{
+		while (*(sentences[i]) != '\0')
+		{
+			while (*sentences[i] != ' ')
+			{
+				reverse.push(*sentences[i]);
+				(*sentences[i])++;
+			}
+
+			while (reverse.empty() == false)
+			{
+				*(sentences_result[i]) = reverse.top();
+				reverse.pop();
+				(*sentences_result[i])++;
+			}
+
+			*(sentences_result[i]) = ' ';
+			(*sentences_result[i])++;
+		}
+	}
+
+	//결과 출력
+	for (int i = 0; i < num; i++)
+	{
+		std::cout << sentences[i] << std::endl;
+	}
+
+
+	//위 동적할당 해제
+	for (int i = 0; i < num; i++)
+	{
+		delete sentences[i];
+	}
+	delete[] sentences;
+
+	return 0;
+}
+
