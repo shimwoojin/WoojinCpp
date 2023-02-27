ecdfbeb50c28a9f0c558c684eb81567d7174778e
심우진 <ggoggal@gmail.com>
2023-02-27 13:37:48 +0900

    20230227

--- Baekjoon/00_NoCategory/2178_graph.cpp
+++ Baekjoon/00_NoCategory/2178_graph.cpp
@@ -63,3 +63,186 @@ int main(void)
 
 	return 0;
 }
+
+// 무방향
+//class Graph
+//{
+//public:
+//    Graph(int nodeCount)
+//        : nodeCount(nodeCount)
+//    {
+//        graph = new bool* [nodeCount];
+//        for (int i = 0; i < nodeCount; i++)
+//            graph[i] = new bool[nodeCount];
+//
+//        for (int i = 0; i < nodeCount; i++)
+//        {
+//            for (int j = 0; j < nodeCount; j++)
+//            {
+//                graph[i][j] = 0;
+//            }
+//        }
+//
+//        visit = new int[nodeCount];
+//        for (int i = 0; i < nodeCount; i++)
+//            visit[i] = 0;
+//    }
+//
+//    ~Graph()
+//    {
+//        for (int i = 0; i < nodeCount; i++)
+//            delete[] graph[i];
+//        delete graph;
+//
+//        delete[] visit;
+//    }
+//
+//    void MakeLink(int x, int y)
+//    {
+//        if (nodeCount <= x || nodeCount <= y)
+//        {
+//            cout << "용량초과" << endl;
+//            return;
+//        }
+//        else
+//        {
+//            graph[y][x] = 1;
+//            graph[x][y] = 1;
+//        }
+//    }
+//
+//    void Display()
+//    {
+//        for (int i = 0; i < nodeCount; i++)
+//        {
+//            for (int j = 0; j < nodeCount; j++)
+//            {
+//                cout << graph[i][j] << " ";
+//            }
+//            cout << endl;
+//        }
+//    }
+//
+//    int BFS(int start, int target)
+//    {
+//        q.push(start);
+//        int depth = 1;
+//        int back = -1;
+//
+//        while (q.empty() == false)
+//        {
+//            int now = q.front();
+//
+//            if (now == target) break;
+//
+//            q.pop();
+//
+//            if (visit[now] == 0)
+//            {
+//                visit[now] = 1;
+//                //cout << now << " ";
+//
+//                for (int i = 0; i < nodeCount; i++)
+//                {
+//                    if (graph[now][i] == 1 && visit[i] == 0)
+//                    {
+//                        q.push(i);
+//                    }
+//                }
+//                if (back == -1 || back == now)
+//                {
+//                    ++depth;
+//                    back = q.back();
+//                }
+//            }
+//        }
+//
+//        return depth;
+//    }
+//public:
+//    int count = 0;
+//
+//private:
+//    // 노드의 개수가 5개라면 
+//    bool** graph = nullptr; // graph[5][5]
+//    int* visit = nullptr;  // visit[5]
+//
+//    int nodeCount = 0;
+//    queue<int> q;
+//
+//};
+
+//int main()
+//{
+//    cin.tie(NULL);
+//    ios::sync_with_stdio(false);
+//
+//    int n, m;
+//    int node_count = 0;
+//    cin >> n >> m;
+//
+//    node_count = n * m;
+//
+//    Graph graph(node_count);
+//
+//    string* str = new string[n];
+//
+//    for (int i = 0; i < n; i++)
+//    {
+//        cin >> str[i];
+//    }
+//
+//    for (int i = 0; i < n; i++)
+//    {
+//        for (int j = 0; j < m; j++)
+//        {
+//            if (str[i][j] == '1')
+//            {
+//                int left = j - 1;
+//                int right = j + 1;
+//                int down = i + 1;
+//                int up = i - 1;
+//
+//                if (left >= 0)
+//                {
+//                    if (str[i][left] == '1')
+//                    {
+//                        graph.MakeLink(i * m + j, i * m + left);
+//                    }
+//                }
+//
+//                if (right < m)
+//                {
+//                    if (str[i][right] == '1')
+//                    {
+//                        graph.MakeLink(i * m + j, i * m + right);
+//                    }
+//                }
+//
+//                if (up >= 0)
+//                {
+//                    if (str[up][j] == '1')
+//                    {
+//                        graph.MakeLink(i * m + j, up * m + j);
+//                    }
+//                }
+//
+//                if (down < n)
+//                {
+//                    if (str[down][j] == '1')
+//                    {
+//                        graph.MakeLink(i * m + j, down * m + j);
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//    int depth = graph.BFS(0, node_count - 1);
+//
+//    cout << depth;
+//
+//    delete[] str;
+//
+//    return 0;
+//}
