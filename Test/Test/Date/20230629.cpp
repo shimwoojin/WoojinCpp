3598253f38532462a666317d062af1b6f151cf70
심우진 <ggoggal@gmail.com>
2023-06-29 11:38:41 +0900

    20230629

---
+++ Test/Test/Date/20230629.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <functional>
+using namespace std;
+
+void Test(int MyInt, float MyFloat, char MyChar)
+{
+	cout << "MyInt : " << MyInt << "MyFloat : " << MyFloat << "MyChar : " << MyChar << endl;
+}
+
+int main()
+{
+	void (*MyFuncPtr)(int, float, char) = Test;
+
+	MyFuncPtr(1,3.14f,'c');
+
+	//void (*MyFuncPtr2)(int, char) = std::bind(&Test, std::placeholders::_1, 3.14f, std::placeholders::_1);
+	
+	std::function<void(int, char)> MyFuncPtr3 = std::bind(&Test, std::placeholders::_1, 3.14f, std::placeholders::_2);
+
+	MyFuncPtr3(3, 'c');
+
+	return 0;
+}
