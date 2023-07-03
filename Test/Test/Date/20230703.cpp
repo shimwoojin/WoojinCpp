7b40e188651d22faa281078000a1109285c67c71
woojin Shim <swj6w7@daum.net>
2023-07-03 20:30:38 +0900

    20230703

---
+++ Test/Test/Date/20230703.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Parent
+{
+public:
+	Parent()
+	{
+		CanThisCallByOverrideVersion();
+	}
+
+	virtual void CanThisCallByOverrideVersion()
+	{
+		cout << "Parent's Function" << endl;
+	}
+};
+
+class Child : public Parent
+{
+public:
+	Child()
+		: Parent()
+	{
+	}
+
+	virtual void CanThisCallByOverrideVersion() override
+	{
+		cout << "Child's Function" << endl;
+	}
+};
+
+int main()
+{
+	Parent parent;
+	Child child;
+
+	cout << sizeof(parent) << endl << sizeof(child) << endl;
+
+	return 0;
+}
