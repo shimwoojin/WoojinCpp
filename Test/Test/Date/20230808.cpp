0a34f48fad64fefe0abc36690ba763748b640f8d
심우진 <ggoggal@gmail.com>
2023-08-08 14:54:04 +0900

    20230808

--- Test/Test/Date/20230808.cpp
+++ Test/Test/Date/20230808.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <map>
 using namespace std;
 
 int main()
 {
-	vector<vector<int>> myvec;
+	/*vector<vector<int>> myvec;
 
 	myvec.assign(10, vector<int>());
 
@@ -13,7 +14,32 @@ int main()
 	myvec[7].push_back(3);
 
 	cout << myvec[3][0] << endl;
-	cout << myvec[7][0] << endl;
+	cout << myvec[7][0] << endl;*/
+
+	struct XY
+	{
+		XY(int x, int y)
+		{
+			this->x = x;
+			this->y = y;
+		}
+
+		int x;
+		int y;
+	};
+	
+	map<XY, int> MyMap;
+	
+	XY xy(2, 3);
+
+	/*MyMap.emplace(XY(2, 3), 5);
+
+	cout << MyMap.at(XY(2, 3)) << endl;*/
+	/*{
+		if (MyMap.find(XY(2, 3)) != MyMap.end())
+		{
+		}
+	}*/
 
 
 	return 0;
