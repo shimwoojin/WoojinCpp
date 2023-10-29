89944ea3764ff156bf23c64566ea7edb2ed8f781
woojin Shim <swj6w7@daum.net>
2023-10-29 09:57:34 +0900

    231029

---
+++ Programmers/WithFriends/Sort/42747.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+
+    sort(citations.begin(), citations.end(), greater<int>());
+
+    for (int i = 0; i < citations.size(); i++)
+    {
+        if (i + 1 <= citations[i])
+        {
+            ++answer;
+        }
+    }
+
+    return answer;
+}
