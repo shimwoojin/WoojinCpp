b243d153a1354490d5262d614722e80082f5e485
ShimWoojin <ggoggal@gmail.com>
2023-12-19 17:23:27 +0900

    231219

---
+++ Test/Date/2312/231219.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <functional>
+#include <typeinfo>
+
+void Hello()
+{
+	std::cout << "Hello"<< std::endl;
+}
+
+struct FunctorTest
+{
+	void operator()()
+	{
+		std::cout << "FunctorTest" << std::endl;
+	}
+};
+
+int main()
+{
+	using namespace std;
+
+	void (*FnPtr)() = Hello;
+	FnPtr();
+
+	{
+		FnPtr = []() {
+			std::cout << "Lambda" <<std::endl;
+			};
+		FnPtr();
+
+		FnPtr = +[]() {
+			std::cout << "Lambda +Version" << std::endl;
+			};
+		FnPtr();
+	}
+
+	//FnPtr = FunctorTest();	//Error
+	std::function<void()> FnPtr2 = FunctorTest();
+
+	auto LambdaTest = +[]() {};		//+가 있으면 함수 포인터로
+	LambdaTest = []() {};
+
+	auto LambdaTest2 = []() {};	//+가 없으면 class lambda
+	//LambdaTest2 = []() {};	//Error
+
+	auto t = FunctorTest();
+	t();
+
+	cout << typeid(FnPtr).name() << endl;
+	cout << typeid(FnPtr2).name() << endl;
+	cout << typeid(LambdaTest).name() << endl;
+	cout << typeid(LambdaTest2).name() << endl;
+	cout << typeid(t).name() << endl;
+
+	return 0;
+}
